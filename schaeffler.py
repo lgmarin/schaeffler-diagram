dc20043096fdb7d04a1018b1c0ef42a3ad3d88af
Luiz Marin <luiz_guimarin@hotmail.com>
2018-07-25 22:45:36 -0300

    Add labels for lines and regions of the diagram

--- schaeffler.py
+++ schaeffler.py
@@ -16,6 +16,10 @@ class Schaeffler:
         self.sf_lines = [[(3.1, 0), (0, 8.1)], [(20.46, 2.76), (0, 20.06)], [(12.23, 0), (40, 9.17)],\
          [(6.96, 0), (34.65, 30)], [(26.37, 4.70), (0, 25.73)], [(15.06, 7.34), (37.52, 30)],\
          [(15.82, 6.72), (40.0, 28.38)], [(16.68, 5.97), (40, 23.45)], [(17.74, 5.08), (40, 19.84)], [(18.64, 4.33), (40, 14.53)]]
+        # Labels for the lines and regions [(x,y), "Label", fontsize]
+        self.sf_labels = [[0.1, 1, "M+F", 9], [31.5, 28, "0%", 9], [31.5, 22.5, "5%", 9], [31.5, 26, "10%", 9],\
+         [31.5, 19, "20%", 9], [31.5, 16, "40%", 9], [31.5, 12, "80%", 9], [31.5, 7.5, "100% F", 9],\
+         [3, 7, "Martensita", 10], [11, 2, "M+F", 10], [14, 24, "Austenita", 10], [16, 7.5, "A+M+F", 10]]
 
     def calc_creq(self, material):
         creq = material['Cr'] + material['Mo'] + 1.5 * material['Si'] + 0.5 * material['Others']
@@ -44,30 +48,36 @@ class Schaeffler:
         print("Metal de Adição: {0}, Creq: {1:.1f}%, Nieq: {2:.1f}%, Tipo: {3}".format(aws_e309['name'], creq[1], nieq[1], aws_e309['type'])) 
         # Weld Metal (Welded Joint)
         print("Junta soldada: Creq: {0:.1f}%, Nieq: {1:.1f}%".format(weld[0], weld[1]))
-        plot = Plot(creq, nieq, self.sf_lines)
+        plot = Plot(creq, nieq, self.sf_lines, self.sf_labels)
         plot.create_plot(weld)
 
 class Plot:
-    def __init__(self, creq, nieq, sf_lines):
+    def __init__(self, creq, nieq, sf_lines, sf_labels):
         # Load Graph BKG
         self.creq = creq
         self.nieq = nieq
         self.fig, self.ax = plt.subplots()
         self.sf_lines = sf_lines
+        self.sf_labels = sf_labels
 
-    def create_plot(self, weld, show_lines=True):
-        self.ax.grid(color='k', linestyle='--', linewidth=1)
+    def create_plot(self, weld, show_lines=True, show_lines_labels=True):
+        # Set plt params
+        self.ax.grid(b=True, which='major')
+        plt.axis('scaled')
+        self.ax.set(xlim=(0, 40), ylim=(0, 30), xlabel='Cromo Equivalente (%)', ylabel='Níquel Equivalente (%)', title="Diagrama de Schaeffler")
+        
         # Create background lines
         if show_lines:
             # Create a mplib collections from the lines points
             lc = matplotlib.collections.LineCollection(self.sf_lines, colors='k', linewidths=1)
             self.ax.add_collection(lc)
-        plt.axis('scaled')
-        plt.xlim(0,40) #<-- set the x axis limits
-        plt.ylim(0,30) #<-- set the y axis limits
+        if show_lines_labels:
+            for i, value in enumerate(self.sf_labels, 0):
+                # Add labels
+                plt.text(value[0], value[1], value[2], fontsize=value[3])
+        
         self.ax.plot(self.creq[0], self.nieq[0], 'b<', label="Metal de Base")
         self.ax.plot(self.creq[1], self.nieq[1], 'b>', label="Metal de Adição")
-        self.ax.set(xlabel='Cromo Equivalente (%)', ylabel='Níquel Equivalente (%)', title="Diagrama de Schaeffler")
         self.ax.plot(weld[0], weld[1], 'bo', label="Solda")
         plt.show()
 
